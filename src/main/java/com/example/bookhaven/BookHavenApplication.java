be8449ecce2e56b721fb74b581b05145ea73452c
Isadora Lucena <66965789+Isadoralucena@users.noreply.github.com>
2024-06-25 13:26:03 -0300

    add initial files with dependencies and db config

---
+++ src/main/java/com/example/bookhaven/BookHavenApplication.java
@@ -0,0 +1,18 @@
+package com.example.bookhaven;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHavenApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHavenApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
